721406c28dd6ecf44738dea8ff56d6b32e09de27
KooTaehyeon <vcro12123@naver.com>
2023-05-10 18:24:48 +0900

    style study

--- App.tsx
+++ App.tsx
@@ -3,9 +3,18 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    // style={{ flexDirection: 'row' }}
+
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>서울</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>맑음</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +22,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'tomato',
+  },
+  city: {
+    flex: 1.2,
+
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    color: '#fff',
+    fontSize: 68,
+    fontWeight: '600',
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  temp: {
+    fontSize: 178,
+    marginTop: 50,
+  },
+  description: {
+    fontSize: 60,
+    marginTop: -30,
   },
 });
